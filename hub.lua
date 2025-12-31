b8b841630fffb2d7bbd80b6e90962f80d224adec
script <154348722+ScriptsHub07@users.noreply.github.com>
2025-12-31 12:58:21 -0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -178,7 +178,7 @@ local function scanAllFastOverheadTemplates()
             print("🔎 Scan template " .. scannedCount)
             
             -- Procurar pela GUI dentro do template
-            local gui = template:FindFirstChild("GUI")
+            local gui = template:FindFirstChild("AnimalOverhead")
             if gui then
                 print("   ✅ GUI encontrada")
                 
